ba69222da14e0ba1c30ab70e6d71a15564ceb11d
Amir <aimir@local>
2022-10-05 22:48:56 +0300

    modified threading code - simplify + get thread count from command line

--- threads.py
+++ threads.py
@@ -1,21 +1,20 @@
 from multiprocessing import Pool
 from subprocess import Popen, PIPE
-import os
-from tqdm import tqdm
+from os.path import exists
+from sys import argv
 
 def run_branch(machine):
     fname = machine + b'.txt'
-    if os.path.exists(fname):
+    if exists(fname):
         with open(fname, 'r') as f:
             contents = f.read()
-        if contents.endswith('done\r\n') or contents.endswith('done\n'):
+        if contents.endswith('done\n') or contents.endswith('done\n'):
             return
-    proc = Popen(['gen.exe'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    proc.communicate(machine + b'\r\n')
-
-with open('starts.txt','rb') as f:
-    machines = f.read().splitlines()
+    proc = Popen(['./bbchallenge_seed_duplication.out'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    proc.communicate(machine + b'\n')
 
 if __name__ == "__main__":
-    with Pool(4) as p:
-        value = list(tqdm(p.imap(run_branch, machines), total=len(machines)))
+    with open('starts.txt','rb') as f:
+        machines = f.read().splitlines()
+    with Pool(int(argv[1])) as p:
+        p.map(run_branch, machines)
